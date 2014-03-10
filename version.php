04e4972306752c7bdd9a813aa480859c0de4cbb2
valery <valery.fremaux@gmail.com>
2014-03-10 23:05:34 +0100

    small fixes

--- version.php
+++ version.php
@@ -5,4 +5,4 @@ $plugin->version = 2014021502;
 $plugin->requires = 2013040500;
 $plugin->maturity = MATURITY_BETA;
 $plugin->release = '2.5.0 (Build 2014021500)';
-$plugin->dependencies = array('local_cms' => '2014130100');
+$plugin->dependencies = array('local_cms' => '2014031000');
