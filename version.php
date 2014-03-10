273d2d4b6ec91408b49eab733dcb63a50108469e
valery <valery.fremaux@gmail.com>
2014-03-10 23:02:52 +0100

    cleanup and comments

--- version.php
+++ version.php
@@ -3,6 +3,7 @@
 $plugin->component = 'block_cms_navigation';
 $plugin->version = 2014021502;
 $plugin->requires = 2012110900;
-$plugin->release = '2.4.0 (Build 2014021500)';
 $plugin->maturity = MATURITY_BETA;
-$plugin->dependencies = array('local_cms' => '2014130100');
+$plugin->release = '2.4.0 (Build 2014021500)';
+$plugin->dependencies = array('local_cms' => '2014031000');
+
