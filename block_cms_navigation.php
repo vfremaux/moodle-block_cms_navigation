7715c96d0bd825e52908150bf625a3e6bda5cca3
valery <valery.fremaux@gmail.com>
2018-01-31 14:46:38 +0100

    Add missing branches

--- block_cms_navigation.php
+++ block_cms_navigation.php
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Block main class.
  *
@@ -25,6 +23,8 @@ defined('MOODLE_INTERNAL') || die();
  * @author Moodle 2.x Valery Fremaux <valery.fremaux@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+defined('MOODLE_INTERNAL') || die();
+
 require_once($CFG->dirroot.'/local/cms/locallib.php');
 
 if (!defined('FRONTPAGECMS')) define ('FRONTPAGECMS', 29);
