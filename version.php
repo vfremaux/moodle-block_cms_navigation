9cff62a0be9cb0a183fc764f125fcb76bdb35ea9
valery <valery.fremaux@gmail.com>
2018-01-31 14:47:13 +0100

    Add missing branches

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2014031000;
-$plugin->requires = 2016120500;
-$plugin->maturity = MATURITY_BETA;
+$plugin->requires = 2017050500;
+$plugin->maturity = MATURITY_RC;
 $plugin->component = 'block_cms_navigation';
-$plugin->release = '3.2.0 (Build 2014021500)';
+$plugin->release = '3.3.0 (Build 2014021500)';
 $plugin->dependencies = array('local_cms' => '2014031000');
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0000';
+$plugin->codeincrement = '3.3.0000';
