057fc703e500519e4219ca1b27e11c0c06587595
valery <valery.fremaux@gmail.com>
2018-05-22 13:48:29 +0200

    Pushing new M35 versions

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2015071000;
-$plugin->requires = 2017110800;
+$plugin->requires = 2018042700;
 $plugin->maturity = MATURITY_RC;
 $plugin->component = 'block_cms_navigation';
-$plugin->release = '3.4.0 (Build 2015071000)';
+$plugin->release = '3.5.0 (Build 2015071000)';
 $plugin->dependencies = array('local_cms' => '2014031000');
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.4.0000';
+$plugin->codeincrement = '3.5.0000';
