d5a8a744af835e6325e27287d80b1a25e7fe9c48
valery <valery.fremaux@gmail.com>
2018-09-29 12:18:25 +0200

    Fix GRDP

--- lang/en/block_cms_navigation.php
+++ lang/en/block_cms_navigation.php
@@ -1,7 +1,29 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * @package   block_cms_navigation
+ * @copyright 2015 Valery Fremaux
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 $string['cms_navigation:addinstance'] = 'Can add an instance to course';
 $string['cms_navigation:myaddinstance'] = 'Can add an instance to My page';
+
+// Privacy
+$string['privacy:metadata'] = 'The Audit Quiz Results block does not directly store any personal data about any user.';
 
 $string['backtohome'] = 'Back to home';
 $string['backtocourse'] = 'Back to course';
@@ -15,4 +37,5 @@ $string['managemenus'] = 'Manage menus';
 $string['managepages'] = 'Manage pages';
 $string['manageitems'] = 'Manage CMS items';
 $string['menuname'] = 'Menu name';
+$string['nomenus'] = 'No CMS menu in the site';
 
--- lang/fr/block_cms_navigation.php
+++ lang/fr/block_cms_navigation.php
@@ -1,7 +1,29 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * @package   block_cms_navigation
+ * @copyright 2015 Valery Fremaux
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 $string['cms_navigation:addinstance'] = 'Peut ajouter une instance au cours';
 $string['cms_navigation:myaddinstance'] = 'Peut ajouter une instance à Ma page';
+
+// Privacy
+$string['privacy:metadata'] = 'Le bloc Navigation CMS ne détient directement aucune donnée relative aux utilisateurs.';
 
 $string['backtohome'] = 'Retour à l\'accueil';
 $string['backtocourse'] = 'Retour au cours';
@@ -15,3 +37,5 @@ $string['managemenus'] = 'Gérer les menus';
 $string['managepages'] = 'Gérer les pages';
 $string['choosemenu'] = 'Choisir le menu';
 $string['menuname'] = 'Nom du bloc';
+$string['nomenus'] = 'Aucun menu dans ce site';
+
